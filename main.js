3ca7f3f2fdf7f3f89aa58789935d9a182dfd054a
Harish Marihalkar <124078515+friday-18@users.noreply.github.com>
2023-02-12 14:14:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+const navMenu = document.getElementById('nav-menu'),
+        toggleMenu = document.getElementById('nav-toggle'),
+        closeMenu = document.getElementById('nav-close')
+
+
+
+toggleMenu.addEventListener('click',()=>{
+    navMenu.classList.toggle('show')
+})
+
+
+closeMenu.addEventListener('click',()=>{
+    navMenu.classList.remove('show')
+})
+
+
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    navMenu.classList.remove('show')
+}
+
+navLink.forEach(n => n.addEventListener('click',linkAction))
+
+const sections = document.querySelectorAll('section[id]')
+
+window.addEventListener('scroll', scrollActive)
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight ){
+            document.querySelectorAll('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
+        }else{
+            document.querySelectorAll('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
+        }
+    })
+
+}
